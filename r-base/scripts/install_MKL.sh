252e9ce87395ecfa2a5eaf032f97148fb76110e7
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2020-12-08 15:03:29 -0500

    Updated dockers

--- r-base/scripts/install_MKL.sh
+++ r-base/scripts/install_MKL.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+export DEBIAN_FRONTEND=noninteractive
+
 apt-get update \
   && apt-get install -y --no-install-recommends wget gnupg2 ca-certificates
 
@@ -11,4 +13,17 @@ wget  --no-check-certificate https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-
 sh -c 'echo deb https://apt.repos.intel.com/mkl all main > /etc/apt/sources.list.d/intel-mkl.list'
 
 # Install MKL
-apt-get update -y && apt-get install -y --no-install-recommends -o=Dpkg::Use-Pty=0 intel-mkl
+apt-get update -y && apt-get install -q -y --no-install-recommends -o=Dpkg::Use-Pty=0 intel-mkl
+
+
+## update alternatives
+update-alternatives --install /usr/lib/x86_64-linux-gnu/libblas.so     \
+                    libblas.so-x86_64-linux-gnu      /usr/lib/x86_64-linux-gnu/libmkl_rt.so 50
+update-alternatives --install /usr/lib/x86_64-linux-gnu/libblas.so.3   \
+                    libblas.so.3-x86_64-linux-gnu    /usr/lib/x86_64-linux-gnu/libmkl_rt.so 50
+update-alternatives --install /usr/lib/x86_64-linux-gnu/liblapack.so   \
+                    liblapack.so-x86_64-linux-gnu    /usr/lib/x86_64-linux-gnu/libmkl_rt.so 50
+update-alternatives --install /usr/lib/x86_64-linux-gnu/liblapack.so.3 \
+                    liblapack.so.3-x86_64-linux-gnu  /usr/lib/x86_64-linux-gnu/libmkl_rt.so 50
+
+echo "MKL_THREADING_LAYER=GNU" >> /etc/environment
