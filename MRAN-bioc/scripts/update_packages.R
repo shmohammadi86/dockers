055e8a8b12c15650a34f82ebcdfb821d697a8013
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2020-12-10 06:18:56 -0500

    Updates

--- MRAN-bioc/scripts/update_packages.R
+++
@@ -1,6 +0,0 @@
-install.packages(
-    c("evaluate", "digest", "highr", "markdown", "stringr", "yaml", 
-       "htmltools", "caTools", "knitr", "base64enc","rprojroot", "rmarkdown"
-    ),
-    repos = c(CRAN = "https://cloud.r-project.org/")
-)
